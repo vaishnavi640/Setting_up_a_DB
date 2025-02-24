00480ae251fc290b84ca76a4c7312322c208cb4b
vaishnavi640 <vaishnavikalluri@gmail.com>
2025-02-24 15:21:36 +0530

    completed assignment

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const { resolve } = require('path');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = 3010;
 
 app.use(express.static('static'));
+
+
+
+const DB_URL = 'mongodb+srv://vaishnavikalluris86:goldi9010512346@assignment.dq6ty.mongodb.net/?retryWrites=true&w=majority&appName=assignment';
+
+mongoose.connect(DB_URL)
+.then(()=>{console.log('Mongodb connected Successfully!')})
+.catch(err => console.log('Mongodb connection failed : ',err));
+
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
